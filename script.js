601bd395b5136d24001a74f6fe0ad92011fa1599
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-09-11 17:06:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let mota  = document.querySelector("#next")
+let mota2  = document.querySelector("#progress")
+
+
+mota.addEventListner("click", motapitta)
+
+
+function motapitta(){
+	
+}
