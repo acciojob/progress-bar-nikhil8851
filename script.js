7b03964ab9ace7987e69c2ed7f27f522f9fd9dfb
nikhil8851 <74211221+nikhil8851@users.noreply.github.com>
2024-09-14 13:41:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,53 @@
-//your JS code here. If required.
-let mota  = document.querySelector("#next")
-let mota2  = document.querySelector("#progress")
+const progress = document.getElementById('progress');
+const prev = document.getElementById('prev');
+const next = document.getElementById('next');
+let circul= document.querySelectorAll(".circle")
 
 
-mota.addEventListner("click", motapitta)
+
+let count = 1
 
 
-function motapitta(){
-	
+next.addEventListener("click", () =>{
+    count++;
+    if (count > circul.length) {
+        count = circul.length;
+    }
+    update()
+});
+
+prev.addEventListener("click", () =>{
+    count--;
+    if(count < 1){
+        count= 1;
+    }
+    update()
+});
+
+
+
+function update(){
+    circul.forEach((circul,index) => {
+        if(index< count){
+            circul.classList.add("active") 
+        }else{  
+            circul.classList.remove("active")
+        }
+    })
+    const actives = document.querySelectorAll('.active');
+
+    progress.style.width = ((actives.length - 1) / (circul.length - 1)) * 100 + '%';
+    console.log(actives.length-1);
+    console.log(circul.length-1)
+
+    if (count === 1) {
+        prev.disabled = true;
+    } else if (count === circul.length) {
+        next.disabled = true;
+    } else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
+
+
 }
